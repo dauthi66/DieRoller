6fe052f6c3d26b6581d80bad1b846d0299ff4715
Kris <dauthi66@gmail.com>
2022-01-16 14:31:50 -0800

    added constructor for die class

--- DieRoller/Die.cs
+++ DieRoller/Die.cs
@@ -9,10 +9,14 @@ namespace DieRoller
     //represents a single six sided die (1-6)
     internal class Die
     {
+        public Die()
+        {
+            Roll();
+        }
         /// <summary>
         /// the current face up value of the die to use
         /// </summary>
-        public byte FaceUpValue { get; set; }
+        public byte FaceUpValue { get; private set; }
         /// <summary>
         /// True if the user chooses to hold the die
         /// </summary>
@@ -24,9 +28,9 @@ namespace DieRoller
         /// <returns>Returns a new random number 1-6</returns>
         public byte Roll()
         {
-            Random random = new Random();
-            byte FaceValue = (byte)random.Next(1, 7);
-            return FaceValue;
+            Random random = new();
+            byte FaceUpValue = (byte)random.Next(1, 7);
+            return FaceUpValue;
         }
     }
 }
