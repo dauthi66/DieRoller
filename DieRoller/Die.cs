e41c37f350a339c987d7602fb87c8dfb3ae9588b
Kris <dauthi66@gmail.com>
2022-01-16 14:21:44 -0800

    Added roll method for the dieRoller class

---
+++ DieRoller/Die.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieRoller
+{
+    //represents a single six sided die (1-6)
+    internal class Die
+    {
+        /// <summary>
+        /// the current face up value of the die to use
+        /// </summary>
+        public byte FaceUpValue { get; set; }
+        /// <summary>
+        /// True if the user chooses to hold the die
+        /// </summary>
+        public bool IsHeld {  get; set; }
+
+        /// <summary>
+        /// "Rolls" a die by returning a random number 1-6 
+        /// </summary>
+        /// <returns>Returns a new random number 1-6</returns>
+        public byte Roll()
+        {
+            Random random = new Random();
+            byte FaceValue = (byte)random.Next(1, 7);
+            return FaceValue;
+        }
+    }
+}
